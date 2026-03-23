6068419c84b5e8cbcec68cd1df2f45bb36e46824
Paavo Penttila <thxmuffe@hotmail.com>
2026-03-23 21:13:33 +0200

    Fix framepage

--- src/webapp/pages/frame_page.cljs
+++ src/webapp/pages/frame_page.cljs
@@ -162,7 +162,7 @@
          [:> Box {:className (str "detail-page" (when fullscreen? " detail-page-fullscreen"))}
           (when-not fullscreen?
             [top-controls from-page roster-id saga-id])
-          [frame-renderer/render-frame active-frame
+          [frame-renderer/render-frame (:frameId active-frame)
            {:clickable? false
             :media-nav? true
             :image-fit "contain"}]
